f94ae27c7d10e95c4ffec18fc248dc7d6d53bbb4
Peter Landwehr <pmlandwehr@gmail.com>
2014-10-22 10:46:00 -0400

    Fixed spacing for PEP 8 compliance

--- generate_narrative_tag_pickle.py
+++ generate_narrative_tag_pickle.py
@@ -1,13 +1,12 @@
 import pickle
 import nltk
 
-data=pickle.load(open('all_narrative_cleanup.txt','rb'))
+data = pickle.load(open('all_narrative_cleanup.txt', 'rb'))
 
-tagged_narrative=[]
-print('total length %d'% len(data))
+tagged_narrative = []
+print('total length %d' % len(data))
 
 for element in data:
     tagged_narrative.append(nltk.pos_tag(element))
         
-pickle.dump(tagged_narrative,open('all_narrative_tagged.txt','wb'))
-    
+pickle.dump(tagged_narrative,open('all_narrative_tagged.txt', 'wb'))
